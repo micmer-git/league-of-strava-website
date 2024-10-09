f383995116817c680f9394d8f5ac892a6a9ab8d0
mmerelli <michelemerelli.8@gmail.com>
2024-10-09 08:12:06 +0200

    locally kind a works

--- app.py
+++ app.py
@@ -521,7 +521,10 @@ def leaderboard():
 
     return render_template('leaderboard.html', users=sorted_users)
 
+
+
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # Creates the tables if they don't exist
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Use PORT if defined, else default to 5000
+    app.run(host='0.0.0.0', port=port, debug=False)
